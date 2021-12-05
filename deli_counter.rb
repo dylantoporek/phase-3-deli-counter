b52882997e80abc74dcdfa97e015cc25cc2946f8
DylanToporek <dylantoporek@gmail.com>
2021-12-05 16:02:44 -0500

    commit tree 824f243821d26b31311566d490a3bd7e2062bb9f

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    names_with_index = katz_deli.map.with_index do |name, index|
+    index_to_display = index + 1
+        "#{index_to_display}. #{name}"
+    end
+    names_with_index_string = names_with_index.join(" ")
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    elsif 
+        puts "The line is currently: #{names_with_index_string}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli.push(name) 
+    index_to_display = katz_deli.length
+    puts "Welcome, #{name}. You are number #{index_to_display} in line."
+
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    elsif
+        next_up = katz_deli[0] + "."
+        puts "Currently serving #{next_up}"
+        katz_deli.shift  
+    end
+end
